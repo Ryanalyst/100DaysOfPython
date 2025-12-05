086b7f28b0e1988257d8554c8b7ab4cd81bc88b7
Ryan Waldrop <ryanalyst95@gmail.com>
2025-12-05 11:45:50 -0600

    Day one

---
+++ Day 1/1. Working with Variables.py
@@ -0,0 +1,14 @@
+# We can print characters to the screen with the print function. You need to include quotation marks surrounding what we want to print to the screen.
+# print("Hello world!")
+
+# We can also move to a new line and continue outputting to the new line by using \n. This is an escape character that tells the function it is not a part of the actual input
+# print("Hello world!\nHello world~\nHello there!")
+
+# By adding a + symbol between different strings we can concatenate or combine them into one output. But keep in mind that it does not add a space for you, you need to do that manually
+#print("Hello " + "Ryan!")
+
+# We can gather input from the user by using python's input function. We can then use their input in our code
+#fav_snack = input("What is your favorite snack: ")
+#print("You love " + fav_snack)
+
+git config --global user.email "ryanalyst95@gmail.com"
